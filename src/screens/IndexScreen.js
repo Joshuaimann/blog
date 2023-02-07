12da0f77c5be31a8bba44571983c590ff74e0e6d
Joshuaiman <jsmith3@wpi.edu>
2023-02-07 12:13:15 -0500

    initial setup

---
+++ src/screens/IndexScreen.js
@@ -0,0 +1,11 @@
+import { View, Text, StyleSheet } from "react-native";
+
+const IndexScreen = () => {
+  return (
+    <View>
+      <Text></Text>
+    </View>
+  );
+};
+
+export default IndexScreen;
